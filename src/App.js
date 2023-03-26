3bcb9ca13ece3b0c0da0736602082fd87f3b05a8
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2023-03-26 18:44:20 +0300

    finishing the solution of (list rendering challenge) ;

--- src/App.js
+++ src/App.js
@@ -6,6 +6,34 @@ import Post from "./Post";
 import SideMenu from "./SideMenu";
 
 function App() {
+  const posts = [
+    {
+      id: 1,
+      postName: "أكاديمية ترميز",
+      postBody: "أكاديمية مخصصة لتعليم البرمجة",
+    },
+
+    {
+      id: 2,
+      postName: "Hello",
+      postBody: "This is the second body",
+    },
+
+    {
+      id: 3,
+      postName: "the third post",
+      postBody: "This is the third body",
+    },
+    {
+      id: 4,
+      postName: "the third post",
+      postBody: "This is the third body",
+    },
+  ];
+
+  const postsList = posts.map((post) => {
+    return <Post key={post.id} postName={post.postName} />;
+  });
   return (
     <div className="App">
       <Header />
@@ -16,22 +44,7 @@ function App() {
           style={{ width: "60%", display: "flex", justifyContent: "center" }}
         >
           {/* == POSTS CONTAINER == */}
-          <div style={{ width: "70%" }}>
-            <Post
-              postName="أكاديمية ترميز"
-              postBody="أكاديمية مخصصة لتعليم البرمجة"
-            />
-
-            <Post postName="Hello" postBody="This is the second body" />
-
-            <Post postName="the third post" postBody="This is the third body" />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-          </div>
+          <div style={{ width: "70%" }}>{postsList}</div>
 
           <div style={{ width: "30%", marginTop: "25px" }}>
             <SideMenu />
--- src/SideMenu.js
+++ src/SideMenu.js
@@ -1,9 +1,54 @@
 import TagButton from "./TagButton";
 
 export default function SideMenu() {
+  const categories = [
+    {
+      uniqueId: 1,
+      title: "آخر المقالات",
+      c: (
+        <div>
+          <span>😀😀😀</span>
+        </div>
+      ),
+    },
+
+    {
+      uniqueId: 2,
+      title: "الأكثر قراءة",
+      c: (
+        <div>
+          <img
+            style={{ width: "100px" }}
+            src="https://www.thespruce.com/thmb/ClRANI4jTWhkGeNhvJtArRhlGSA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-difference-between-trees-and-shrubs-3269804-hero-a4000090f0714f59a8ec6201ad250d90.jpg"
+          />
+        </div>
+      ),
+    },
+
+    {
+      uniqueId: 3,
+      title: "مميز",
+      c: (
+        <>
+          <h1>Click Me</h1>
+          <span>👍🏻</span>
+        </>
+      ),
+    },
+  ];
+
+  const categoriesList = categories.map((cat) => {
+    return (
+      <TagButton key={cat.uniqueId} title={cat.title}>
+        {cat.c}
+      </TagButton>
+    );
+  });
+
   return (
     <div style={{ border: "solid teal 5px" }}>
-      <TagButton title="آخر المقالات">
+      {categoriesList}
+      {/* <TagButton title="آخر المقالات">
         <div>
           <span>😀😀😀</span>
         </div>
@@ -21,7 +66,7 @@ export default function SideMenu() {
         <h1>Click Me</h1>
         <span>👍🏻</span>
       </TagButton>
-      <TagButton title="مقالات الأسبوع" />
+      <TagButton title="مقالات الأسبوع" /> */}
       {/* <TagButton />
       <TagButton />
       <TagButton />
