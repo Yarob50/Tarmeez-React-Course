0f329f4ea3f5acfab48cbeeea59965a81c3c0790
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2023-03-31 23:39:46 +0300

    done

--- src/MyForm.js
+++ src/MyForm.js
@@ -4,18 +4,29 @@ export default function MyForm() {
   //   const [nameInput, setNameInput] = useState("");
   //   const [emailInput, setEmailInput] = useState("");
 
-  const [formInputs, setFormInput] = useState({ name: "", email: "" });
+  const [formInputs, setFormInput] = useState({
+    name: "",
+    email: "",
+    age: "",
+    generalInfo: "",
+    isStudent: false,
+  });
+
+  function handleCheckBoxChanged(event) {
+    setFormInput({ ...formInputs, isStudent: event.target.checked });
+  }
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
+        console.log(formInputs);
       }}
     >
       <label>Name:</label>
       <input
         value={formInputs.name}
         onChange={(event) => {
-          setFormInput({ name: event.target.value });
+          setFormInput({ ...formInputs, name: event.target.value });
         }}
       />
 
@@ -25,11 +36,39 @@ export default function MyForm() {
       <input
         value={formInputs.email}
         onChange={(event) => {
-          setFormInput({ email: event.target.value });
+          //   const newFormInput = { ...formInputs };
+          //   newFormInput.email = event.target.value;
+          //   setFormInput(newFormInput);
+
+          setFormInput({ ...formInputs, email: event.target.value });
+        }}
+      />
+
+      <label>Age:</label>
+      <input
+        value={formInputs.age}
+        onChange={(event) => {
+          setFormInput({ ...formInputs, age: event.target.value });
         }}
       />
 
       <hr></hr>
+
+      <label>Are you student</label>
+      <input
+        type="checkbox"
+        checked={formInputs.isStudent}
+        onChange={handleCheckBoxChanged}
+      />
+
+      <hr></hr>
+      <label>General Info</label>
+      <textarea
+        value={formInputs.generalInfo}
+        onChange={(event) => {
+          setFormInput({ ...formInputs, generalInfo: event.target.value });
+        }}
+      />
 
       <button>Submit</button>
     </form>
