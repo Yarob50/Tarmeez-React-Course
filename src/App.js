f049cb4a6ebd61829064e75ee259eef5b5260d89
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2023-04-01 22:03:59 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [deviceNameInputValue, setDeviceNameInputValue] = useState("");
+  const [devices, setDevices] = useState([
+    "iphone",
+    "mac",
+    "samsung",
+    "windows",
+  ]);
+  const devicesList = devices.map((device) => {
+    return <li>{device}</li>;
+  });
+
+  function handleAddClick() {
+    // const newDevices = [...devices];
+    // newDevices.push(deviceNameInputValue);
+    // setDevices(newDevices);
+
+    setDevices([...devices, deviceNameInputValue]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ marginTop: "100px", fontSize: "30px" }}>
+      {devicesList}
+
+      <div>
+        <input
+          value={deviceNameInputValue}
+          onChange={(event) => {
+            setDeviceNameInputValue(event.target.value);
+          }}
+          type="text"
+        />
+        <button onClick={handleAddClick}>Add</button>
+      </div>
     </div>
   );
 }
