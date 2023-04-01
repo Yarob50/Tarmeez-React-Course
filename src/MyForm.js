4fda0e6913a1ce3ae80b489f4a391d6a59c073f8
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2023-04-01 03:06:11 +0300

    done

--- src/MyForm.js
+++ src/MyForm.js
@@ -10,6 +10,8 @@ export default function MyForm() {
     age: "",
     generalInfo: "",
     isStudent: false,
+    country: "",
+    status: "",
   });
 
   function handleCheckBoxChanged(event) {
@@ -29,9 +31,7 @@ export default function MyForm() {
           setFormInput({ ...formInputs, name: event.target.value });
         }}
       />
-
       <hr></hr>
-
       <label>Email:</label>
       <input
         value={formInputs.email}
@@ -43,7 +43,6 @@ export default function MyForm() {
           setFormInput({ ...formInputs, email: event.target.value });
         }}
       />
-
       <label>Age:</label>
       <input
         value={formInputs.age}
@@ -51,15 +50,48 @@ export default function MyForm() {
           setFormInput({ ...formInputs, age: event.target.value });
         }}
       />
-
       <hr></hr>
-
       <label>Are you student</label>
       <input
         type="checkbox"
         checked={formInputs.isStudent}
         onChange={handleCheckBoxChanged}
       />
+      <hr></hr>
+      <select
+        value={formInputs.country}
+        onChange={(event) => {
+          setFormInput({ ...formInputs, country: event.target.value });
+        }}
+      >
+        <option>KSA</option>
+        <option>Egypt</option>
+        <option>Syria</option>
+      </select>
+      <hr></hr>
+
+      <div>
+        <input
+          value="student"
+          type="radio"
+          name="status"
+          checked={formInputs.status == "student"}
+          onChange={(event) => {
+            setFormInput({ ...formInputs, status: event.target.value });
+          }}
+        />
+        Student
+        <input
+          value="teacher"
+          type="radio"
+          name="status"
+          checked={formInputs.status == "teacher"}
+          onChange={(event) => {
+            setFormInput({ ...formInputs, status: event.target.value });
+          }}
+        />
+        Teacher
+      </div>
 
       <hr></hr>
       <label>General Info</label>
@@ -69,7 +101,6 @@ export default function MyForm() {
           setFormInput({ ...formInputs, generalInfo: event.target.value });
         }}
       />
-
       <button>Submit</button>
     </form>
   );
