c4a20485e2bcc4a50605e0b8f2814a1e225e9f6e
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2023-04-02 03:28:39 +0300

    done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,58 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
+let nextId = 5;
 function App() {
   const [deviceNameInputValue, setDeviceNameInputValue] = useState("");
   const [devices, setDevices] = useState([
-    "iphone",
-    "mac",
-    "samsung",
-    "windows",
+    { id: 1, name: "iphone" },
+    { id: 2, name: "mac" },
+    { id: 3, name: "samsung" },
+    { id: 4, name: "windows" },
   ]);
   const devicesList = devices.map((device) => {
-    return <li>{device}</li>;
+    return (
+      <li key={device.id}>
+        {device.name}{" "}
+        <button
+          onClick={() => {
+            handleDeleteClick(device.id);
+          }}
+        >
+          Delete
+        </button>
+      </li>
+    );
   });
+
+  function handleDeleteClick(id) {
+    // const newDevices = [...devices];
+
+    // let index = 0;
+    // let selectedIndex = 0;
+
+    // for (let device of newDevices) {
+    //   if (device.id == id) {
+    //     selectedIndex = index;
+    //   }
+    //   index++;
+    // }
+
+    // newDevices.splice(selectedIndex, 1);
+
+    const newDevices = devices.filter((device) => {
+      return device.id !== id;
+    });
+    setDevices(newDevices);
+  }
 
   function handleAddClick() {
     // const newDevices = [...devices];
     // newDevices.push(deviceNameInputValue);
     // setDevices(newDevices);
 
-    setDevices([...devices, deviceNameInputValue]);
+    setDevices([...devices, { id: nextId, name: deviceNameInputValue }]);
+    nextId = nextId + 1;
   }
 
   return (
