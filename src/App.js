d458a3a9768321a30535963d1ed1813d22478a6d
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2023-04-03 17:51:47 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handlePlusClick() {
+    setCount(count + 1);
+    setCount((c) => {
+      return c + 1;
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The count is: {count}</h1>
+      <button onClick={handlePlusClick}>+</button>
     </div>
   );
 }
