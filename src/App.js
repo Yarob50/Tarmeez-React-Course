e12bf22353b6ac9d93d04230ed9afae235581153
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2023-04-28 14:11:43 +0300

    done

--- src/App.js
+++ src/App.js
@@ -6,60 +6,70 @@ import { TodosContext } from "./contexts/todosContext";
 import { useState } from "react";
 
 import { v4 as uuidv4 } from "uuid";
+import MySnackBar from "./components/MySnackBar";
 const theme = createTheme({
-  typography: {
-    fontFamily: ["Alexandria"],
-  },
+	typography: {
+		fontFamily: ["Alexandria"],
+	},
 
-  palette: {
-    primary: {
-      main: "#dd2c00",
-    },
-  },
+	palette: {
+		primary: {
+			main: "#dd2c00",
+		},
+	},
 });
 
 const initialTodos = [
-  {
-    id: uuidv4(),
-    title: "قراءة كتاب",
-    details: "تيسمبتيس يتسبميتس بيمستب",
-    isCompleted: false,
-  },
-  {
-    id: uuidv4(),
-    title: "قراءة كتاب",
-    details: "تيسمبتيس يتسبميتس بيمستب",
-    isCompleted: false,
-  },
-  {
-    id: uuidv4(),
-    title: "قراءة كتاب",
-    details: "تيسمبتيس يتسبميتس بيمستب",
-    isCompleted: false,
-  },
+	{
+		id: uuidv4(),
+		title: "قراءة كتاب",
+		details: "تيسمبتيس يتسبميتس بيمستب",
+		isCompleted: false,
+	},
+	{
+		id: uuidv4(),
+		title: "قراءة كتاب",
+		details: "تيسمبتيس يتسبميتس بيمستب",
+		isCompleted: false,
+	},
+	{
+		id: uuidv4(),
+		title: "قراءة كتاب",
+		details: "تيسمبتيس يتسبميتس بيمستب",
+		isCompleted: false,
+	},
 ];
 
 function App() {
-  const [todos, setTodos] = useState(initialTodos);
-  return (
-    <ThemeProvider theme={theme}>
-      <div
-        className="App"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          background: "#191b1f",
-          height: "100vh",
-          direction: "rtl",
-        }}
-      >
-        <TodosContext.Provider value={{ todos, setTodos }}>
-          <TodoList />
-        </TodosContext.Provider>
-      </div>
-    </ThemeProvider>
-  );
+	const [todos, setTodos] = useState(initialTodos);
+	const [open, setOpen] = useState(true);
+
+	function showHideToast() {
+		setOpen(true);
+		setTimeout(() => {
+			setOpen(false);
+		}, 2000);
+	}
+	return (
+		<ThemeProvider theme={theme}>
+			<div
+				className="App"
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					background: "#191b1f",
+					height: "100vh",
+					direction: "rtl",
+				}}
+			>
+				<MySnackBar open={open} />
+				<TodosContext.Provider value={{ todos, setTodos }}>
+					<TodoList />
+				</TodosContext.Provider>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
---
+++ src/components/MySnackBar.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import MuiAlert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function MySnackBar({ open }) {
+	const action = (
+		<React.Fragment>
+			<IconButton size="small" aria-label="close" color="inherit">
+				<CloseIcon fontSize="small" />
+			</IconButton>
+		</React.Fragment>
+	);
+
+	return (
+		<div>
+			<Stack spacing={2} sx={{ width: "100%" }}>
+				<Snackbar
+					open={open}
+					autoHideDuration={6000}
+					message="Note archived"
+					action={action}
+				>
+					<Alert severity="success" sx={{ width: "100%" }}>
+						This is a success message!
+					</Alert>
+				</Snackbar>
+			</Stack>
+		</div>
+	);
+}
