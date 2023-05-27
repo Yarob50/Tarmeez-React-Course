aaf7a64c052041459933caf4d4bc139c970fe4af
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2023-05-27 14:53:30 +0300

    initial setup (before using reducer)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [firstNumberInput, setFirstNumberInput] = useState(null);
+	const [secondNumberInput, setSecondNumberInput] = useState(null);
+	const [result, setResult] = useState(null);
+
+	// EVENT HANDLERS
+	function handleSumClick() {
+		const result = Number(firstNumberInput) + Number(secondNumberInput);
+		setResult(result);
+	}
+
+	function handleSubClick() {
+		const result = Number(firstNumberInput) - Number(secondNumberInput);
+		setResult(result);
+	}
+
+	function handleMultClick() {
+		const result = Number(firstNumberInput) * Number(secondNumberInput);
+		setResult(result);
+	}
+
+	function handleDivClick() {
+		const result = Number(firstNumberInput) / Number(secondNumberInput);
+		setResult(result);
+	}
+
+	return (
+		<div className="App">
+			<div
+				style={{
+					height: "100vh",
+					display: "flex",
+					justifyContent: "center",
+					flexDirection: "column",
+					alignItems: "center",
+					background: "teal",
+				}}
+			>
+				{/* FIRST INPUT */}
+				<label>First Number</label>
+				<input
+					value={firstNumberInput}
+					onChange={(e) => setFirstNumberInput(e.target.value)}
+				/>
+
+				{/* SECOND INPUT */}
+				<label>Second Number</label>
+				<input
+					value={secondNumberInput}
+					onChange={(e) => setSecondNumberInput(e.target.value)}
+				/>
+
+				<button onClick={handleSumClick}>sum</button>
+
+				<button onClick={handleSubClick}>subtract</button>
+
+				<button onClick={handleMultClick}>multiply</button>
+
+				<button onClick={handleDivClick}>divide</button>
+
+				<hr />
+
+				<h2>{result}</h2>
+			</div>
+		</div>
+	);
 }
 
 export default App;
